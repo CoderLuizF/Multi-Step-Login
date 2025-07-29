3c81db534cdcca5e6e86567f7be625df2e955fd6
CoderLuizF <zzzezizzz@gmail.com>
2025-07-29 16:01:31 -0300

    feat: add basic HTML structure to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     
+      <div className="app">
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>
+            Ficamos felizes com a sua compra, utilize o formulário abaixo para
+            avaliar o produto
+          </p>
+        </div>
+        <div className="form-container">
+          <p>etapas</p>
+          <form>
+            <div className="actions">
+              <button type="button">
+                <span>Voltar</span>
+              </button>
+              <button type="submit">
+                <span>Avançar</span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
