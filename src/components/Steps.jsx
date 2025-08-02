2676e3344e7e250755fc87c7b8f1239ae69cbf65
CoderLuizF <zzzezizzz@gmail.com>
2025-08-02 08:54:04 -0300

    feat: add initial HTML structure for stage indicator component

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./Steps.css";
+import { AiOutlineUser, AiOutlineStar } from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
 
-const Steps = () => {
+const Steps = (currentStep) => {
   return (
-    <div>Steps</div>
-  )
-}
+    <div className="steps">
+      <div className="step">
+        <AiOutlineUser />
+        <p>Identificação</p>
+      </div>
+      <div className="step">
+        <AiOutlineStar />
+        <p>Avaliação</p>
+      </div>
+      <div className="step">
+        <FiSend />
+        <p>Envio</p>
+      </div>
+    </div>
+  );
+};
 
-export default Steps
+export default Steps;
